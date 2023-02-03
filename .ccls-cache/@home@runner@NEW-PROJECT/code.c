b319b33d007b3768f69985e86e62cd4ad9943ede
rafisys <115602435+rafisys@users.noreply.github.com>
2023-02-03 02:23:11 +0000

    Compatetive

--- .ccls-cache/@home@runner@NEW-PROJECT/code.c
+++ .ccls-cache/@home@runner@NEW-PROJECT/code.c
@@ -2,17 +2,32 @@
 
 int main()
 {
-  int i,j,n;
-  printf("Enter number ");
+  int i,j,r,n,k;
   scanf("%d",&n);
-  for(i=1;i<=2*n-1;i++)
+  for(i=1,r=0;i<=2*n-1;i++)
     {
-      
+      i<=n? r++ : r--;
+      k=n;
       for(j=1;j<=2*n-1;j++)
         {
-          if(j<=i)
-          printf("*");
+          if(j>=i && j<=2*n-r)
+          {
+          printf("k");
+            }
+                else
+      {
+        if(j<n)
+        {
+          printf("%d",k);
+            k--;
         }
+        else{
+          k++;
+          printf("%d",k) ; 
+        }
+      }
+        }
+
       printf("\n");
     }
   return 0;
