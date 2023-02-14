6ef2390be49ee3da0078325f56ecf6f996317a50
rafisys <115602435+rafisys@users.noreply.github.com>
2023-02-14 03:06:03 +0000

    star

--- .ccls-cache/@home@runner@NEW-PROJECT/code.c
+++ .ccls-cache/@home@runner@NEW-PROJECT/code.c
@@ -1,35 +1,16 @@
 #include<stdio.h>
 
-int main()
+ int main()
 {
-  int i,j,r,n,k;
-  scanf("%d",&n);
-  for(i=1,r=0;i<=2*n-1;i++)
-    {
-      i<=n? r++ : r--;
-      k=n;
-      for(j=1;j<=2*n-1;j++)
-        {
-          if(j>=i && j<=2*n-r)
-          {
-          printf("%d",k);
-            }
-         
-          else
-      {
-        if(j<n)
-        {
-          printf("%d",k);
-            k--;
-        }
-        else{
-          k++;
-          printf("%d",k) ; 
-        }
-      }
-        }
-
-      printf("\n");
-    }
-  return 0;
+ int i,j,k,n;
+ scanf("%d",&n);
+ for(i=1;i<=2*n-1;i++)
+ { 
+  k=n;
+   for(j=1;j<=2*n-1;j++)
+   { 
+     printf("k");
+   }
+     printf("\n");
+  }
 }
--- .ccls-cache/@home@runner@NEW-PROJECT/main.c
+++ .ccls-cache/@home@runner@NEW-PROJECT/main.c
@@ -2,35 +2,31 @@
 
 int main()
 {
-  int i,j,r,n,k;
-  scanf("%d",&n);
-  for(i=1,r=0;i<=2*n-1;i++)
-    {
-      i<=n? r++ : r--;
-      k=n;
-      for(j=1;j<=2*n-1;j++)
-        {
-          if(j>=i && j<=2*n-r)
-          {
-          printf("%d",k );
-            }
-           else
+int i,j,n,k,r;
+scanf("%d",&n);
+ for(i=1,r=0;i<=2*n-1;i++)
+  {
+    i<=n?r++:r--;
+    k=n;
+    for(j=1;j<=2*n-1;j++)
+      {
+      if(j>r && j<=2*n-r)
+      {
+        printf("%d",k);
 
-                {
-                     if(j>n)
-                     {
-                       printf("%d",k );
-                          k--;
-                      }
-         else
-              {
-           k++;
-          printf("%d",k ) ; 
+      }        
+      else{
+        if(j<n){
+          printf("%d",k);
+          k--;
+        }
+        else{
+          k++;
+          printf("%d",k);
         }
       }
-        }
-
-      printf("\n");
-    }
-  return 0;
-}
+        
+      }
+    printf("\n");
+  }
+  }  
