c03af272cf68fc8f317acacadaadca853df1c48e
rafisys <115602435+rafisys@users.noreply.github.com>
2023-02-17 07:13:26 +0000

    1

--- .ccls-cache/@home@runner@NEW-PROJECT/pyramidd.c
+++ .ccls-cache/@home@runner@NEW-PROJECT/pyramidd.c
@@ -3,14 +3,14 @@
 int main()
 {
   int i,j;
-  for(i=2;i<=5;i++)
+  for(i=1;i<=5;i++)
     {
       for(j=1;j<=9;j++)
         {
-          if(j>=6-i && j<=i+4)
-           printf("*");
+          if(j>=6-i)
+            printf("*");
           else
-           printf("\n"); 
+            printf("*");
         }
       printf("\n");
     }
--- .ccls-cache/@home@runner@NEW-PROJECT/pyrimid.c
+++ .ccls-cache/@home@runner@NEW-PROJECT/pyrimid.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-int main(){
-
+int main()
+{
   int i,j;
-  for(i=1;i<=5;i++)
+  for(i=2;i<=5;i++)
     {
       for(j=1;j<=9;j++)
         {
-          if(j>=6-i&& j<=i+4)
-            printf("*");
+          if(j>=6-i && j<=i+4)
+           printf("*");
           else
-            printf(" ");
+           printf("\n"); 
         }
       printf("\n");
     }
